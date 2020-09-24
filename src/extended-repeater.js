b81e1db7030a3b384ec5c3eec9ad3fa2140eff73
IvannaHrytsiuk <54635863+IvannaHrytsiuk@users.noreply.github.com>
2020-09-24 12:47:46 +0300

    feat: add solving

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+  let addition;
+  if (!options.separator) {
+    options.separator = '+';
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = '|';
+  }
+  addition = options.addition !== undefined ? options.addition : '';
+  options.additionRepeatTimes--;
+  
+  while (options.additionRepeatTimes > 0) {
+    addition += options.additionSeparator + options.addition;
+    options.additionRepeatTimes--;
+  }
+
+  res = str = str + addition;
+  options.repeatTimes--;
+
+  while (options.repeatTimes > 0) {
+    res += options.separator + str;
+    options.repeatTimes--;
+  }
+
+  return res;
 };
   
