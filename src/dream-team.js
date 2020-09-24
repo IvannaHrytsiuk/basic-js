ad3ad8cfb3794408e29dca37f20256d4d03c971e
IvannaHrytsiuk <54635863+IvannaHrytsiuk@users.noreply.github.com>
2020-09-24 12:47:07 +0300

    feat: add solving

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+ let teamname = '';
+ try {
+    members.map(mem => {
+        if (typeof mem !== 'string') {
+          return '';
+        }
+        return mem.trim().toUpperCase()
+      })
+      .sort()
+      .forEach(mem => {
+      if (mem[0]) {
+          teamname += mem[0];
+        }
+      })
+    return teamname;
+  } catch (err) {
+    return false;
+  }
 };
